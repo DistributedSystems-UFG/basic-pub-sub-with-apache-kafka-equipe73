a34a120ac4c38fc5ec67099b77f3c745869bbc12
gustav0leal <gustavoleal@discente.ufg.br>
2023-02-24 12:35:15 -0300

    final version

--- producer.py
+++ producer.py
@@ -1,17 +1,19 @@
 from kafka import KafkaProducer
 from const import *
 import sys
+import random
 
-try:
-    topic = sys.argv[1]
-except:
-    print ('Usage: python3 producer <topic_name>')
-    exit(1)
+# try:
+#     topic = sys.argv[1]
+# except:
+#     print ('Usage: python3 producer <topic_name>')
+#     exit(1)
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
+    x = random.randint(1,2) 
+    msg = 'My ' + str(i) + 'st message for topic ' + str(x)
+    producer.send(str(x), value=msg.encode())
     print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
 
 producer.flush()
